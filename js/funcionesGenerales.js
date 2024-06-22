07e6d80fc79c02768a055097dc2c44f45ddc10a5
MartinVazquez1982 <martin.vazquezaris@gmail.com>
2024-06-22 15:22:43 -0300

    :sparkles: partial render finished

--- js/funcionesGenerales.js
+++ js/funcionesGenerales.js
@@ -13,11 +13,21 @@ document.addEventListener("DOMContentLoaded", function () {
   // Partial render content
   
   const main = document.querySelector('main')
+
+  // Script que deben ejecutar otras paginas
   const script = {
     "noticias": "js/newsletter.js",
     "eventos": "js/tabladinamica.js"
   }
+
+  const titles = {
+    "inicio": "Lenguajes de Programacion",
+    "lenguajes": "Tipos de Lenguajes",
+    "noticias": "Noticias",
+    "eventos": "Eventos"
+  }
   
+  // Crear un cartel de error
   function crearError(pagina){
     const element = document.createElement('section')
     element.classList.add('sc-conteiner-info')
@@ -28,6 +38,7 @@ document.addEventListener("DOMContentLoaded", function () {
     return element
   }
 
+  // Carga la nueva pagina
   async function cargarPagina(pagina) {
     try {
       const response = await fetch(`html/${pagina}.html`)
@@ -37,7 +48,7 @@ document.addEventListener("DOMContentLoaded", function () {
         if (scriptBorrar !== null){
           scriptBorrar.remove()
         }
-        if (pagina === 'eventos' || pagina === 'noticas'){
+        if (pagina === 'eventos' || pagina === 'noticias'){
           const codigo = document.createElement('script')
           codigo.src = script[pagina]
           codigo.id = 'script-funcionalidad'
@@ -54,17 +65,37 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
+  function actualizarRuta(ruta){
+    document.title = titles[ruta]
+    window.history.pushState({ pagina: ruta }, `${ruta}`, `/${ruta}`)
+  }
+
+  // Revisa cual es la proxima pagina a carga
   function proxContenido(event){
     const pagina = event.target.id
-
+    actualizarRuta(pagina)
     cargarPagina(pagina)
   }
 
+  // Le asigna a cada boton del nav, la pagina a la que redirige
   document.querySelectorAll('.nav-link').forEach( (button) => { button.addEventListener('click', (event) => { proxContenido(event) }) } )
+  
+  window.addEventListener('popstate', function(event) {
+    const pagina = event.state?.pagina
+    if (pagina){
+      document.title = titles[pagina]
+      cargarPagina(pagina) 
+    }
+  })
+
+  // Carga el inicio
+  actualizarRuta('inicio')
   cargarPagina('inicio') 
 
   
-  //Funcionalidad del tema de la pagina (Modo oscuro y modo claro)
+
+  // Funcionalidad del tema de la pagina (Modo oscuro y modo claro)
+
   const btn_tema = document.querySelector("#btn-modo-color");
   let temaOscuro = true;
 
